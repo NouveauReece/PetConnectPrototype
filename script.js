9ebe8a55cbef4eb971716a2cf95c6c1a7645ae95
NouveauReece <nouveaureece@protonmail.com>
2025-03-30 04:12:12 -0400

    😞 Sigh it's always the trivial things

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ async function nextStep(step, metric, val, button) {
     try {
         button.innerText = "Loading...";
         button.classList.add("disabled");
-        console.log(`Sending data: metric=${metric}, val=${val}`);
         const response = await fetch(`https://www.tedxiu.com/_functions/petprototypescan?metric=${metric}&val=${val}`, {
             method: "post",
             headers: {
